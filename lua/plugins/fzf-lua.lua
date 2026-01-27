c69338f1a1d4a501e81db8c68dff411479db6937
Václav Jež <jez.vasek@gmail.com>
2026-01-27 19:59:31 +0100

    add lazygit.nvim, setup send to quickfix list for fzf lua

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,15 +1,24 @@
 return {
-  "ibhagwan/fzf-lua",
-  -- volitelně závislost na ikonách
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local fzf = require("fzf-lua")
-    fzf.setup({})
+    "ibhagwan/fzf-lua",
+    -- volitelně závislost na ikonách
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local fzf = require("fzf-lua")
+        fzf.setup({
+            keymap = {
+                fzf = {
+                    ["ctrl-q"] = "select-all+accept",
+                },
+            },
+        })
 
-    -- Klávesové zkratky
-    vim.keymap.set("n", "<leader><leader>", fzf.files, { desc = "Najít soubory" })
-    vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Hledat text v souborech" })
-    vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Přepnout buffer" })
-    vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Nápověda" })
-  end,
+        -- Klávesové zkratky
+        vim.keymap.set("n", "<leader><leader>", fzf.files, { desc = "Najít soubory" })
+        vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Hledat text v souborech" })
+        vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Přepnout buffer" })
+        vim.keymap.set("n", "<leader>fh", fzf.help_tags, { desc = "Nápověda" })
+
+        vim.keymap.set("n", "<leader>fd", fzf.diagnostics_document, { desc = "LSP chyby (aktuální soubor)" })
+        vim.keymap.set("n", "<leader>fD", fzf.diagnostics_workspace, { desc = "LSP chyby (projekt)" })
+    end,
 }
